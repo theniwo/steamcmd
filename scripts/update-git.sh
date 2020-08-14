3e68b6b67660a1d8e516f72137ce32fe5e2bc433
theniwo <accounts.niwo@mailbox.org>
2020-08-14 11:36:45 +0200

    Auto commit

--- scripts/update-git.sh
+++ scripts/update-git.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-FRIENDLYNAME=Gobuntu
-CONTAINERNAME=gobuntu
-DOCKERREPO=theniwo
-DOCKERIMAGE=gobuntu
-DOCKERTAG=latest
+PARAMETER="$1"
 
-cd /root/Settings/Linux/scripts/docker/$CONTAINERNAME
+SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/env
+
+cd $DIR/..
+
 function main(){
 	logger -i -t $CONTAINERNAME "Adding files"
 	git add .
@@ -33,4 +40,9 @@ function main(){
 	    logger -i -t $CONTAINERNAME "Pushing to git unsuccessful"
 	  fi
 }
+
+if [[ $PARAMETER == "--force" ]] || [[ $PARAMETER == "-f" ]]; then
+  echo "Forcing Commit"
+  date +%Y%m%d%H%M%S > $DIR/../CHANGEFILE
+fi
 main
--- scripts/update-hub.sh
+++ scripts/update-hub.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-FRIENDLYNAME=Gobuntu
-CONTAINERNAME=gobuntu
-DOCKERREPO=theniwo
-DOCKERIMAGE=gobuntu
-DOCKERTAG=latest
-cd /root/Settings/Linux/scripts/docker/$CONTAINERNAME
+
+SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/env
+cd $DIR/..
+
 
 	echo "Committing to docker hub"
 	docker commit $(docker inspect --format='{{.ID}}' $CONTAINERNAME) $DOCKERREPO/$DOCKERIMAGE:$DOCKERTAG && docker push $DOCKERREPO/$DOCKERIMAGE:$DOCKERTAG
@@ -15,13 +21,3 @@ cd /root/Settings/Linux/scripts/docker/$CONTAINERNAME
 		echo "$FRIENDLYNAME has failed to commit and automatically push to docker hub"
 		logger -i -t $CONTAINERNAME "$FRIENDLYNAME has failed to commit and automatically push to docker hub"
 	fi
-	#echo "Committing to docker hub"
-	#docker commit $(docker inspect --format='{{.ID}}' ) theniwo/gobuntu:latest && docker push theniwo/gobuntu:latest
-	#if [ $? -eq 0 ] ; then
-		#echo "Gobuntu has commited and automatically pushed to docker hub"
-		#logger -i -t gobuntu "Gobuntu has commited and automatically pushed to docker hub"
-	#else
-		#echo "Gobuntu has failed to commit and automatically push to docker hub"
-		#logger -i -t gobuntu "Gobuntu has failed to commit and automatically push to docker hub"
-	#fi
-#
